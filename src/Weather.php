91c69a57c7b53729c31dbfd3fb47a9fb4d6de05f
zzl307 <qq1013446945@126.com>
2019-01-03 01:50:46 +0000

    Apply fixes from StyleCI

--- src/Weather.php
+++ src/Weather.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the zzl307/weather.
+ *
+ * (c)zzl307 <qq1013446945@126.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ * with this source code in the file LICENSE.
+ */
+
 namespace Zzl307\Weather;
 
 use GuzzleHttp\Client;
@@ -8,51 +17,52 @@ use Zzl307\Weather\Exceptions\InvalidArgumentException;
 
 class Weather
 {
-	protected $key;
-	protected $guzzleOptions = [];
+    protected $key;
 
-	public function __construct(string $key)
-	{
-		$this->key = $key;
-	}
+    protected $guzzleOptions = [];
 
-	public function getHttpClient()
-	{
-		return new Client($this->guzzleOptions);
-	}
+    public function __construct(string $key)
+    {
+        $this->key = $key;
+    }
 
-	public function setGuzzleOptions(array $options)
-	{
-		$this->guzzleOptions = $options;
-	}
+    public function getHttpClient()
+    {
+        return new Client($this->guzzleOptions);
+    }
 
-	public function getWeather($city, string $type = 'base', string $format = 'json')
-	{
-		$url = 'https://restapi.amap.com/v3/weather/weatherInfo';
+    public function setGuzzleOptions(array $options)
+    {
+        $this->guzzleOptions = $options;
+    }
 
-		if (!\in_array(\strtolower($format), ['xml', 'json'])) {
-			throw new InvalidArgumentException('Invalid response format: '.$format);
-		}
+    public function getWeather($city, string $type = 'base', string $format = 'json')
+    {
+        $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
 
-		if (!\in_array(\strtolower($type), ['base', 'all'])) {
-			throw new InvalidArgumentException('Invalid type value(base/all): '.$type);
-		}
+        if (!\in_array(\strtolower($format), ['xml', 'json'])) {
+            throw new InvalidArgumentException('Invalid response format: '.$format);
+        }
 
-		$query = array_filter([
-			'key' => $this->key,
-			'city' => $city,
-			'output' => $format,
-			'extensions' => $type
-		]);
+        if (!\in_array(\strtolower($type), ['base', 'all'])) {
+            throw new InvalidArgumentException('Invalid type value(base/all): '.$type);
+        }
 
-		try {
-			$response = $this->getHttpClient()->get($url, [
-				'query' => $query
-			])->getBody()->getContents();
+        $query = array_filter([
+            'key' => $this->key,
+            'city' => $city,
+            'output' => $format,
+            'extensions' => $type,
+        ]);
 
-			return 'json' === $format ? \json_decode($response, true) : $response;
-		} catch (\Exception $e) {
-			throw new HttpException($e->getMessage(), $e->getCode(), $e);
-		}
-	}
+        try {
+            $response = $this->getHttpClient()->get($url, [
+                'query' => $query,
+            ])->getBody()->getContents();
+
+            return 'json' === $format ? \json_decode($response, true) : $response;
+        } catch (\Exception $e) {
+            throw new HttpException($e->getMessage(), $e->getCode(), $e);
+        }
+    }
 }
--- tests/WeatherTest.php
+++ tests/WeatherTest.php
@@ -1,4 +1,14 @@
 <?php
+
+/*
+ * This file is part of the zzl307/weather.
+ *
+ * (c)zzl307 <qq1013446945@126.com>
+ *
+ * This source file is subject to the MIT license that is bundled.
+ * with this source code in the file LICENSE.
+ */
+
 namespace Zzl307\Weather\Tests;
 
 use GuzzleHttp\Client;
